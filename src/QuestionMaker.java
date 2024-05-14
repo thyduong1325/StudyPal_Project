9bf0b39750616411b59a291ac9211ad2618319d9
DUT <142745058+ComS-2023@users.noreply.github.com>
2024-05-14 08:09:21 -0500

    Update QuestionMaker.java

--- src/QuestionMaker.java
+++ src/QuestionMaker.java
@@ -1,75 +1,92 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * The QuestionMaker class is responsible for reading questions from a file
  * and creating an array of Question objects based on the file content.
  */
 public class QuestionMaker {
-
-    /**
-     * Reads questions from the specified file and creates an array of Question objects.
-     * 
-     * @param filename the name of the file containing the questions.
-     * @return an array of Question objects.
-     * @throws IOException if there is an error reading the file.
-     */
-    public static Question[] getQuestions(String filename) throws IOException {
-        // Create a list to hold the questions temporarily
-        ArrayList<Question> questions = new ArrayList<Question>();
-
-        // Use a BufferedReader to read the file line by line
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        String line;
-
-        // Read each line of the file
-        while ((line = br.readLine()) != null) {
-            // Split the line into parts using ';' as the delimiter
-            String[] parts = line.split(";");
-
-            // The first part indicates the type of question
-            String type = parts[0];
-
-            // Create the appropriate Question object based on the type
-            if (type.equals("TF")) {
-                // Create a True/False question and add it to the list
-                String questionText = parts[1];
-                boolean answer = Boolean.parseBoolean(parts[2]);
-                int points = Integer.parseInt(parts[3]);
-                QuestionTF tfQuestion = new QuestionTF(questionText, answer, points);
-                questions.add(tfQuestion);
-            } else if (type.equals("MC")) {
-                // Create a Multiple Choice question
-                String questionText = parts[1];
-                int numChoices = Integer.parseInt(parts[2]);
-                String[] choices = new String[numChoices];
-
-                // Extract the choices from the parts array
-                for (int i = 0; i < numChoices; i++) {
-                    choices[i] = parts[3 + i];
+    public static Question[] getQuestions(File file) {
+    	Question[] questions = new Question[countQuestions(file)];
+        try {
+            // Scan the file
+            Scanner scanner = new Scanner(file);
+            int quesNum = 0;
+            
+            // Transform file into Question object
+            while (scanner.hasNextLine()) {
+            	// Take each input line
+                String line = scanner.nextLine();
+                String[] parts = line.split(";");
+                
+                // Declaire the type of question
+                String type = parts[0];
+                
+                // True False Question
+                if (type.equals("TF")) {
+                	// Analyze inputs
+                    String question = parts[1];
+                    boolean answer = Boolean.parseBoolean(parts[2]);
+                    int points = Integer.parseInt(parts[3]);
+                    
+                    // Add the question to the main arraylist
+                    questions[quesNum] = new QuestionTF(question, answer, points);
+                } 
+                else if (type.equals("MC")) {
+                	// Analyze inputs
+                    String question = parts[1];
+                    int numChoices = Integer.parseInt(parts[2]);
+                    String[] choices = new String[numChoices];
+                    for (int i = 0; i < numChoices; i++) {
+                        choices[i] = parts[3 + i];
+                    }
+                    String correctChoice = parts[3 + numChoices];
+                    int points = Integer.parseInt(parts[4 + numChoices]);
+                    
+                    // Add the question to the main arraylist
+                    questions[quesNum] = new QuestionMC(question, choices, correctChoice, points);
+                } 
+                else if (type.equals("FB")) {
+                	// Analyze inputs
+                    String question = parts[1];
+                    String answer = parts[2];
+                    int points = Integer.parseInt(parts[3]);
+                    
+                    // Add the question to the main arraylist
+                    questions[quesNum] = new QuestionFB(question, answer, points);
                 }
-
-                String answer = parts[3 + numChoices];
-                int points = Integer.parseInt(parts[4 + numChoices]);
-                QuestionMC mcQuestion = new QuestionMC(questionText, choices, answer, points);
-                questions.add(mcQuestion);
-            } else if (type.equals("FB")) {
-                // Create a Fill in the Blank question and add it to the list
-                String questionText = parts[1];
-                String answer = parts[2];
-                int points = Integer.parseInt(parts[3]);
-                QuestionFB fbQuestion = new QuestionFB(questionText, answer, points);
-                questions.add(fbQuestion);
+                
+                quesNum ++;
             }
+            // Close the scan
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-
-        // Close the BufferedReader
-        br.close();
-
-        // Convert the list of questions to an array and return it
-        Question[] questionsArray = new Question[questions.size()];
-        questions.toArray(questionsArray);
-        return questionsArray;
+        return questions;
     }
+    
+    private static int countQuestions(File file) {
+    	int result = 0;
+    	try {
+        	// Scan the file
+            Scanner scanner = new Scanner(file);
+            
+            // Transform file into Question object
+            while (scanner.hasNextLine()) {
+            	// Count lines
+            	result ++;
+                scanner.nextLine();
+            }
+            // Close the scan
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return result;
+    	
+    }
+    
 }
-
